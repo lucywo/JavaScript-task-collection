a2d453423487e724f9deef09c90b1cfc33a6d4dd
lucywohltorf <lucywohltorf@air-von-lucy.localdomain>
2023-04-06 09:53:42 +0200

    feat: add task 2 operators and datatypes

---
+++ operators-and-datatypes/script.js
@@ -0,0 +1,20 @@
+//prints out my first name on console
+console.log("L" + "U" + "c" +"Y");
+// Error! Make it work
+console.log("Hallo");
+// Error! Make it work
+console.log("That doesn't work");
+// should return true. Change only the operator to fix it.
+console.log(50 + 2 >= 52);
+// should be false. Change only the operator to fix it.
+console.log("333" === 333);
+// Wrong result: Expected 555 not 855. Change only the operator to fix it.
+console.log((600 + 510) / 2);
+// Wrong result: Expected a Number 4 received 22
+console.log(2 + 2);
+
+//returns true when both divisions are equal to 5
+console.log(25 / 5 === 50 / 10);
+
+// oh no it's a number
+console.log(typeof 4.2);
